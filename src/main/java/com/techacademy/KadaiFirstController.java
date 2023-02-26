9e22cc8bf6c25f8a8b7bf864805bb05ec3b61ada
ryutaro-shimota <ryutaro.2000.radiology@gmail.com>
2023-02-26 18:14:26 +0900

    correct KadaiFirstController.java

--- src/main/java/com/techacademy/KadaiFirstController.java
+++ src/main/java/com/techacademy/KadaiFirstController.java
@@ -41,7 +41,7 @@ public class KadaiFirstController {
     @GetMapping("/divide/{val1}/{val2}")  //割り算
     public int calcDivide(@PathVariable int val1, @PathVariable int val2) {
         int res = 0;
-        res = val1 % val2;
+        res = val1 / val2;
         return  res;
         }
 
